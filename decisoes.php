68dfca24b4f0a5ff4c3e021dc721d36466b46b80
Jose Marcio Ferreira da Silva <jmarciosilva@gmail.com>
2023-06-06 10:37:54 -0300

    Comparações em php

--- decisoes.php
+++ decisoes.php
@@ -1,15 +1,18 @@
 <?php 
 
-$idade = 16;
+$idade = 15;
+$numeroPessoas = 2;
 
 echo "=========================================================" . PHP_EOL;
 echo "========================BALADA===========================" . PHP_EOL;
 echo "=========================================================" . PHP_EOL;
 echo "" . PHP_EOL;
-echo "Você só pode entrar se tiver a partir de 18 anos!" . PHP_EOL;
+echo "Você só pode entrar se tiver a partir de 18 anos ou ter 16 anos e estar acompanhado!" . PHP_EOL;
 
 if ($idade >= 18) {
 	echo"Você tem $idade anos - você pode entrar!" . PHP_EOL;
+} elseif (($idade >= 16) && ($numeroPessoas > 1) ) {
+	echo "Você tem apenas $idade anos - e esta acompanhado - Você pode entrar! " . PHP_EOL;
 } else {
 	echo "Você tem apenas $idade anos - Você não pode entrar! " . PHP_EOL;
 }
