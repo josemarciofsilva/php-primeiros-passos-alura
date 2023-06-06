4d80d1487de3ee3c132640bf2e5227bbc657fe90
Jose Marcio Ferreira da Silva <jmarciosilva@gmail.com>
2023-06-06 12:44:59 -0300

    Desafio imprimir numeros impares de 0 ate 100

---
+++ desafios/impares.php
@@ -0,0 +1,19 @@
+<?php 
+echo "=========================================================" . PHP_EOL;
+echo "========================Numeros Impares==================" . PHP_EOL;
+echo "=========================================================" . PHP_EOL;
+echo "" . PHP_EOL;
+echo "Imprimindo numeros impares de 0 a 100";
+echo "" . PHP_EOL;
+
+for ($i=0; $i <= 100 ; $i++) { 
+	if ($i % 2 != 0)  {
+		echo "#$i" . PHP_EOL;
+	}
+}
+
+
+
+
+
+ ?>
