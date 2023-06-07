c55e0ba5a2c95cc2bd7b1761c9ca028a753ae620
Jose Marcio Ferreira da Silva <jmarciosilva@gmail.com>
2023-06-07 05:35:40 -0300

    Varias ordenações no array

--- arrays/notas.php
+++ arrays/notas.php
@@ -16,3 +16,18 @@ $notas = [
 ksort($notas);
 
 var_dump($notas);
+
+// verificar se uma chave existe no array
+
+echo "Marcio fez a prova: " . PHP_EOL; 
+var_dump(array_key_exists('Marcio', $notas)) . PHP_EOL;
+
+echo PHP_EOL;
+
+echo "Alguem tirou 10 na prova: " . PHP_EOL;
+var_dump(in_array(10, $notas));
+
+echo PHP_EOL;
+
+echo "Quem tirou 10 na prova: " . PHP_EOL;
+echo array_search(10, $notas) . PHP_EOL;
