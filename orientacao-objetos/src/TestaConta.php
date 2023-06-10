bed288ff3fcf21f48ad5de49eca24b90914b80d4
Jose Marcio Ferreira da Silva <jmarciosilva@gmail.com>
2023-06-10 12:10:47 -0300

    Criando a classe Conta e criando objetos a partir dela

---
+++ orientacao-objetos/src/TestaConta.php
@@ -0,0 +1,20 @@
+<?php 
+
+require 'src/Conta.php';
+
+$primeiraConta = new Conta();
+$primeiraConta->saldo = 200;
+$primeiraConta->cpfTitular = '123.456.789-10';
+$primeiraConta->nomeTitular = 'Vinicius Dias';
+
+var_dump($primeiraConta); // Verifique que o objeto já possui os dados necessários de uma conta criada
+
+$segundaConta = new Conta();
+$segundaConta->cpfTitular = '987.654.321-10';
+$segundaConta->nomeTitular = 'Patricia';
+$segundaConta->saldo = 1500;
+
+var_dump($primeiraConta); // Verifique que o objeto não teve seus valores alterados
+
+var_dump($segundaConta); // Note que este objeto possui os valores da segunda conta criada
+
