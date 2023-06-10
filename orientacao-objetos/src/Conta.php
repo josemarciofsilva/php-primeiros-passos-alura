0b628c71d801bd9e4530a89b8ca65f6a94af5398
Jose Marcio Ferreira da Silva <jmarciosilva@gmail.com>
2023-06-10 12:30:03 -0300

    Criando metodos sacar e depositar na classe Conta

--- orientacao-objetos/src/Conta.php
+++ orientacao-objetos/src/Conta.php
@@ -7,4 +7,28 @@ class Conta
 	public string $nomeTitular;
 	public float $saldo;
 
+	public function sacar(float $valor): void
+	{
+		if ($valor > $this->saldo) {
+			echo "Saldo Insuficiente";
+		}
+
+		if ($valor > 0) {
+			$this->saldo -= $valor;	
+		}		
+	}
+
+	public function depositar(float $valor): void
+	{
+		if ($valor < 0) {
+			echo "Valor necessita ser positivo!";
+		}
+
+		$this->saldo += $valor;
+	}
+
+
+
+
+
 }
--- orientacao-objetos/src/TestaConta.php
+++ orientacao-objetos/src/TestaConta.php
@@ -1,6 +1,6 @@
 <?php 
 
-require 'src/Conta.php';
+require 'Conta.php';
 
 $primeiraConta = new Conta();
 $primeiraConta->saldo = 200;
@@ -9,12 +9,20 @@ $primeiraConta->nomeTitular = 'Vinicius Dias';
 
 var_dump($primeiraConta); // Verifique que o objeto já possui os dados necessários de uma conta criada
 
+echo PHP_EOL;
+
 $segundaConta = new Conta();
 $segundaConta->cpfTitular = '987.654.321-10';
 $segundaConta->nomeTitular = 'Patricia';
 $segundaConta->saldo = 1500;
 
+$segundaConta->sacar(200);
+
+$primeiraConta->depositar(300);
+
 var_dump($primeiraConta); // Verifique que o objeto não teve seus valores alterados
+
+echo PHP_EOL;
 
 var_dump($segundaConta); // Note que este objeto possui os valores da segunda conta criada
 
